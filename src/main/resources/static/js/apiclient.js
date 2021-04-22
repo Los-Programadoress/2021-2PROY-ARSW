a3f50b264c81b944ce1a9d5f3f14de98736c1a62
César Ortiz <cesar.ortiz@mail.escuelaing.edu.co>
2021-04-22 00:05:59 -0500

    implementando join

--- src/main/resources/static/js/apiclient.js
+++ src/main/resources/static/js/apiclient.js
@@ -2,7 +2,7 @@ var apiclient = (function () {
 
     var addGameRoom = function(gameRoom){
 		return $.ajax({
-            url: "http://lacman.herokuapp.com/lacman",
+            url: "https://lacman.herokuapp.com/lacman",
             type: "POST",
             data: JSON.stringify(gameRoom),
             contentType: "application/json",
@@ -12,14 +12,14 @@ var apiclient = (function () {
     var getPasscodeByName = function (name){
         axios({
             method:'get',
-            url: "http://lacman.herokuapp.com/lacman/"+name,
+            url: "https://lacman.herokuapp.com/lacman/"+name,
         }).then(response=> Module.showPasscode(response.data))
             .catch(error=> console.log(error));
     };
 
     var isPasscode = function(passcode){
         return $.ajax({
-            url: "http://lacman.herokuapp.com/lacman/passcode",
+            url: "https://lacman.herokuapp.com/lacman/passcode",
             type: "POST",
             data: passcode,
             contentType: "application/json",
