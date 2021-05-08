9ac86908920ba4c1c084e5cc4a7682daca0ef186
Maria Alfaro <maria.alfaro@mail.escuelaing.edu.co>
2021-05-08 18:44:24 -0500

    Actualizando controlador

--- src/main/java/edu/eci/arsw/lacman/controller/LacmanController.java
+++ src/main/java/edu/eci/arsw/lacman/controller/LacmanController.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import edu.eci.arsw.lacman.services.LacmanServices;
  */
 @RestController
 @RequestMapping(value = "/lacman")
+@CrossOrigin(origins = "*")
 public class LacmanController {
 
     @Autowired
